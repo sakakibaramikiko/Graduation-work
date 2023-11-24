75800aeb5b54d30c5891468f57d1c07b55193421
sakakibaramikiko <137418326+sakakibaramikiko@users.noreply.github.com>
2023-11-25 08:29:25 +0900

    add Procfile

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
