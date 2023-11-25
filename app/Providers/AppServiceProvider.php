5b20d3f35e51b0dd5234253610c7e41b74afa235
sakakibaramikiko <137418326+sakakibaramikiko@users.noreply.github.com>
2023-11-25 10:58:55 +0900

    AppSericePeoviderのhttps設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if(\APP::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
